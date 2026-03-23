525a6e1176249bb4471a7640f3881c6162c54d40
SoulEater42-cmd <jmingondza07@gmail.com>
2026-03-23 17:01:15 +0100

    User profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
 import './App.css';
 
-import NavBar from './NavBar';
+
+//import NavBar from './NavBar';
 import RecentReviews from './RecentReviews';
 import SubjectsToRate from './SubjectsToRate';
 import RankingPreview from './RankingPreview';
-import Login from './Login';
+import NavBar from './NavBar';
 
-import React, { useState } from 'react';
+import Login from './Login';
+import { useState } from 'react';
+import ProfilePage from './ProfilePage';
+
+
 function App() {
   const [userEmail, setUserEmail] = useState<string | null>(null);
 
-  if(!userEmail){
-    return <Login onLogin={(email) => setUserEmail(email)} />
-  }
+  
 
   return (
-    <>
-    <NavBar />
-    <h1 className='greetings'>Bienvenue, {userEmail}!</h1>
-    <RecentReviews/>
-    <SubjectsToRate/>
-    <RankingPreview/>
-    </>
+    <> {!userEmail ? (
+        // Si pas d'email -> on affiche le Login
+        <Login onLogin={(email) => setUserEmail(email)} />
+      ) : (
+        // Si l'email existe -> on affiche le site
+        <>
+        
+          <NavBar/>
+          <h1 className='greetings'>Bienvenue, {userEmail}!</h1>
+          <RecentReviews/>
+          <SubjectsToRate/>
+          <RankingPreview/>
+          <ProfilePage/>
+        </>
+      )}</>
+     
+   
   );
 }
 
